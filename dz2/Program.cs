9822578bc697a20c915445039c696eac67bafeb0
Muhammad <type.92@mail.ru>
2023-02-18 02:01:46 +0300

    END

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -5,7 +5,7 @@
 // A (3,6,8); B (2,1,-7) -> 15,84
 // A (7,-5,0); B (1,-1,9) -> 11,53
 
-double Dist (double x,double y,double z)
+double Dist3D (double x,double y,double z)
 {
     double result = Math.Sqrt(Math.Pow(x, 2)+ Math.Pow(y,2) + Math.Pow(z,2));
     return Math.Round(result, 2);
@@ -20,5 +20,5 @@ for(int i = 0; i < dot.Length; i++)
     dot[i] = Convert.ToDouble(Console.ReadLine());
 }
 
-double dist = Dist(dot[0]-dot[3],dot[1]-dot[4],dot[2]-dot[5]);
+double dist = Dist3D(dot[0]-dot[3],dot[1]-dot[4],dot[2]-dot[5]);
 Console.WriteLine($"Растояние = {dist}");
